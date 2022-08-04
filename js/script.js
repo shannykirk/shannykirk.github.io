82698cab4713cf936f52ad32aadfabadbdf402b5
annaboone <anna.boone@startribune.com>
2022-08-04 12:55:05 -0500

    add clicking on project functionality

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-$('#intro').typeIt({
-	speed: 130,
-     autoStart: false
-})
-.tiType('anna marie boone');
-$('.ti-cursor').remove();
+$( document ).ready(function() {
+    
+    // show project name on hover
 
-$(function(){
-    $("#menu").click(function () {
-        $(".slide-content").slideToggle("medium");
-      $('#nav-icon3').toggleClass('open');
+    $( "div.row a" ).hover(
+      function() {
+        $( this ).find("h3").css({
+            opacity: 1,
+        });
+      }, function() {
+        $( this ).find("h3").css({
+            opacity: 0,
+        });
+      }
+    );
+
+    // 
+    $( "div.row a" ).each(function() {
+        $(this).click(function() {
+            var getClass = this.className.toString();
+            var projectExpanded = $("div.work-expanded." + getClass);
+            $(projectExpanded).addClass("show");
+            $("div.work").addClass("hide");
+        });
     });
-});
 
-$(".clickme").click(function() {
-	var classes = ['comic', 'colorfade', 'swing', 'flip'];
-	var randomnumber = Math.floor(Math.random() * classes.length); 
-	$('#intro').removeClass();
-	$("#intro").toggleClass(classes[randomnumber]);
-}); 
-	
+    $( "p.back" ).each(function() {
+        $(this).click(function() {
+            $("body").removeClass('shortened');
+            $("div.work-expanded").removeClass("show");
+            $("div.work").removeClass("hide");
+        });
+    });
+
+
+});
